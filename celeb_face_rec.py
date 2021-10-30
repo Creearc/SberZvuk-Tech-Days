45445e9dbbedf2169aabb64714d9ede26755695c
Alexandr <knxandr@rambler.ru>
2021-10-30 20:24:52 +0500

    Update celeb_face_rec.py

--- celeb_face_rec.py
+++ celeb_face_rec.py
@@ -300,8 +300,8 @@ if __name__ == '__main__':
              
             yourself_representation = model.predict(img_pixels)[0,:]
             name = np.argmin(yourself_representation)
-            cv2.putText(img, name, (box[0], box[1]), 
-                         cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+            cv2.putText(img, str(name), (box[0], box[1]), 
+                         cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2, cv2.LINE_AA)
             print(name)
 ##            df['similarity'] = df['face_vector_raw'].apply(findCosineSimilarity)
 ##            df = df.sort_values(by=['similarity'], ascending=True)
