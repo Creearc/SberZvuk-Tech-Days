c900438bac39ebc6776fc0e64873a1411dc5784c
Alexandr <knxandr@rambler.ru>
2021-10-31 14:55:12 +0500

    0

--- get.py
+++ get.py
@@ -5,7 +5,7 @@ import sys
 PATH = '\\'.join(sys.argv[0].split('\\')[:-1])
 
 ftp = FTP()
-HOST = '192.168.68.202'
+HOST = '192.168.68.128'
 #HOST = '192.168.137.164'
 PORT = 21
 
@@ -14,7 +14,7 @@ ftp.connect(HOST, PORT)
 print(ftp.login(user='alexandr', passwd='9'))
 
 
-ftp.cwd('~/SberZvuk-Tech-Days/opencv-face-recognition/output_0')
+ftp.cwd('~/SberZvuk-Tech-Days/opencv-face-recognition/output')
 #ftp.cwd('~/SberZvuk-Tech-Days/opencv-face-recognition')
 
 #for i in range(28, 36):
