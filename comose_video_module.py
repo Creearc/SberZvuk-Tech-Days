cf0bd7314a1b57cd02dd33d0fbbafa829638ee79
Pavel <slivnitsin1997@mail.ru>
2021-10-31 11:40:25 +0500

    2

---
+++ comose_video_module.py
@@ -0,0 +1,12 @@
+from moviepy.editor import *
+
+def composit(audio_name, video_name):
+    videoclip = VideoFileClip(video_name)
+    audioclip = AudioFileClip(audio_name)
+
+    new_audioclip = CompositeAudioClip([audioclip])
+    videoclip.audio = new_audioclip
+    videoclip.write_videofile("new_filename.mp4")
+
+if __name__ == "__main__":
+    composit("clean.wav", "output.mp4")
