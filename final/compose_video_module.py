3650992408e6ff8dbb019e127069ed7ee116bc9e
Alexandr <knxandr@rambler.ru>
2021-10-31 12:56:37 +0500

    Update compose_video_module.py

--- final/compose_video_module.py
+++ final/compose_video_module.py
@@ -1,14 +1,14 @@
 from moviepy.editor import *
 
 
-def composit(audio_name, video_name):
+def composit(audio_name, video_name, output_name):
     videoclip = VideoFileClip(video_name)
     audioclip = AudioFileClip(audio_name)
 
     new_audioclip = CompositeAudioClip([audioclip])
     videoclip.audio = new_audioclip
-    videoclip.write_videofile("new_filename.mp4")
+    videoclip.write_videofile(output_name)
 
 
 if __name__ == "__main__":
-    composit("clean.wav", "output.mp4")
+    composit("clean.wav", "output.mp4", 'result.mp4')
