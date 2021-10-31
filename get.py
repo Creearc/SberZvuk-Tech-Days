36e39fb79f67cff4a327d5372b640d6e827c9ba8
Alexandr <knxandr@rambler.ru>
2021-10-31 15:13:48 +0500

    1

--- get.py
+++ get.py
@@ -14,14 +14,14 @@ ftp.connect(HOST, PORT)
 print(ftp.login(user='alexandr', passwd='9'))
 
 
-ftp.cwd('~/SberZvuk-Tech-Days/opencv-face-recognition/output')
+ftp.cwd('~/SberZvuk-Tech-Days/final/models')
 #ftp.cwd('~/SberZvuk-Tech-Days/opencv-face-recognition')
 
 #for i in range(28, 36):
 if True:
   fl = 'embeddings.pickle'
   fl = 'le.pickle'
-  fl = 'recognizer.pickle'
+  #fl = 'recognizer.pickle'
   #fl = 'openface_nn4.small2.v1.t7'
   out = '{}\{}'.format(PATH, fl)
 
